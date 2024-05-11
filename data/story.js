b28c90efdfe0d884b1ad103115c0c16fe48be4e6
Giulia Vilone (CTR) <giulia.vilone@xperi.com>
2024-05-11 13:47:58 +0100

    add food tracker prototype

---
+++ data/story.js
@@ -0,0 +1,767 @@
+var story = {
+ "docName": "Food tracker",
+ "docPath": "P_P_P",
+ "docVersion": 100000001,
+ "ownerName": "",
+ "ownerEmail": "",
+ "authorName": "V_V_N",
+ "authorEmail": "V_V_E",
+ "fileType": "png",
+ "disableInteractions": false,
+ "highlightHotspot": true,
+ "highlightAllHotspots": true,
+ "hideGallery": false,
+ "galleryPageColorsEnabled": true,
+ "galleryDecorNodesEnabled": true,
+ "zoomEnabled": true,
+ "cloud": false,
+ "title": "Food tracker",
+ "layersExist": true,
+ "pages": [
+  {
+   "id": "60:4",
+   "groupIndex": 0,
+   "title": "Giphy Stickers",
+   "image": "giphy-stickers.png",
+   "index": 0,
+   "width": 254,
+   "height": 55,
+   "x": -930,
+   "y": -551,
+   "isFrame": false,
+   "type": "item",
+   "fixedPanels": [],
+   "links": []
+  },
+  {
+   "id": "11:105",
+   "groupIndex": 0,
+   "title": "Sign In",
+   "image": "sign-in.png",
+   "index": 1,
+   "width": 375,
+   "height": 812,
+   "x": -930,
+   "y": -417,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Input and button",
+     "rect": {
+      "x": 24,
+      "y": 509,
+      "width": 327,
+      "height": 74
+     },
+     "index": 0,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 1,
+       "frameIndex": 2,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    },
+    {
+     "name": "07 Profile avatar",
+     "rect": {
+      "x": 270,
+      "y": 56,
+      "width": 81,
+      "height": 110
+     },
+     "index": 1,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 1,
+       "frameIndex": 5,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    }
+   ],
+   "layout": null,
+   "tmpSolitBackground": false
+  },
+  {
+   "id": "37:568",
+   "groupIndex": 0,
+   "title": "Product1",
+   "image": "product1.png",
+   "index": 2,
+   "width": 375,
+   "height": 812,
+   "x": -469,
+   "y": -412,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Icon/Chevron_Left",
+     "rect": {
+      "x": 16,
+      "y": 53,
+      "width": 24,
+      "height": 24
+     },
+     "index": 2,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 2,
+       "frameIndex": 1,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    },
+    {
+     "name": "05 Tab list",
+     "rect": {
+      "x": 153,
+      "y": 676,
+      "width": 65,
+      "height": 59
+     },
+     "index": 3,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 2,
+       "frameIndex": 3,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    }
+   ],
+   "layout": null,
+   "tmpSolitBackground": false
+  },
+  {
+   "id": "1:159",
+   "groupIndex": 0,
+   "title": "Product2",
+   "image": "product2.png",
+   "index": 3,
+   "width": 375,
+   "height": 812,
+   "x": -9,
+   "y": -407,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Button",
+     "rect": {
+      "x": 13,
+      "y": 717,
+      "width": 343,
+      "height": 58
+     },
+     "index": 4,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 3,
+       "frameIndex": 4,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    },
+    {
+     "name": "Icon/Chevron_Left",
+     "rect": {
+      "x": 16,
+      "y": 53,
+      "width": 24,
+      "height": 24
+     },
+     "index": 5,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 3,
+       "frameIndex": 2,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    },
+    {
+     "name": "01 Button - Primary",
+     "rect": {
+      "x": 10,
+      "y": 179,
+      "width": 166,
+      "height": 54
+     },
+     "index": 6,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 3,
+       "frameIndex": 8,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    },
+    {
+     "name": "01 Button - Primary",
+     "rect": {
+      "x": 188,
+      "y": 179,
+      "width": 166,
+      "height": 54
+     },
+     "index": 7,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 3,
+       "frameIndex": 9,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    }
+   ],
+   "layout": null,
+   "tmpSolitBackground": false
+  },
+  {
+   "id": "11:409",
+   "groupIndex": 0,
+   "title": "Dashboard1",
+   "image": "dashboard1.png",
+   "index": 4,
+   "width": 375,
+   "height": 812,
+   "x": 444,
+   "y": -407,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Icon/Chevron_Left",
+     "rect": {
+      "x": 16,
+      "y": 53,
+      "width": 24,
+      "height": 24
+     },
+     "index": 8,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 4,
+       "frameIndex": 3,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    },
+    {
+     "name": "Button",
+     "rect": {
+      "x": 19,
+      "y": 715,
+      "width": 343,
+      "height": 52
+     },
+     "index": 9,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 4,
+       "frameIndex": 11,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    },
+    {
+     "name": "05 Tab list",
+     "rect": {
+      "x": 270,
+      "y": 510,
+      "width": 68,
+      "height": 32
+     },
+     "index": 10,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 4,
+       "frameIndex": 7,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    }
+   ],
+   "layout": null,
+   "tmpSolitBackground": false
+  },
+  {
+   "id": "43:197",
+   "groupIndex": 0,
+   "title": "Profile",
+   "image": "profile.png",
+   "index": 5,
+   "width": 375,
+   "height": 812,
+   "x": 897,
+   "y": -412,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Icon/Chevron_Left",
+     "rect": {
+      "x": 16,
+      "y": 53,
+      "width": 24,
+      "height": 24
+     },
+     "index": 11,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 5,
+       "frameIndex": 10,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    }
+   ],
+   "layout": null,
+   "tmpSolitBackground": false
+  },
+  {
+   "id": "44:85",
+   "groupIndex": 0,
+   "title": "Profile image",
+   "image": "profile-image.png",
+   "index": 6,
+   "width": 24,
+   "height": 24,
+   "x": 1231,
+   "y": -358,
+   "isFrame": false,
+   "type": "item",
+   "fixedPanels": [],
+   "links": []
+  },
+  {
+   "id": "44:37",
+   "groupIndex": 0,
+   "title": "Dashboard2",
+   "image": "dashboard2.png",
+   "index": 7,
+   "width": 375,
+   "height": 812,
+   "x": 1312,
+   "y": -407,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Button",
+     "rect": {
+      "x": 19,
+      "y": 715,
+      "width": 343,
+      "height": 52
+     },
+     "index": 12,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 7,
+       "frameIndex": 11,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    },
+    {
+     "name": "01 Button - Primary",
+     "rect": {
+      "x": 107,
+      "y": 566,
+      "width": 167,
+      "height": 36
+     },
+     "index": 13,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 7,
+       "frameIndex": 4,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    },
+    {
+     "name": "Icon/Chevron_Left",
+     "rect": {
+      "x": 16,
+      "y": 53,
+      "width": 24,
+      "height": 24
+     },
+     "index": 14,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 7,
+       "frameIndex": 4,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    }
+   ],
+   "layout": null,
+   "tmpSolitBackground": false
+  },
+  {
+   "id": "49:43",
+   "groupIndex": 0,
+   "title": "Product3",
+   "image": "product3.png",
+   "index": 8,
+   "width": 375,
+   "height": 812,
+   "x": 1727,
+   "y": -407,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Button",
+     "rect": {
+      "x": 13,
+      "y": 717,
+      "width": 343,
+      "height": 58
+     },
+     "index": 15,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 8,
+       "frameIndex": 4,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    },
+    {
+     "name": "Icon/Chevron_Left",
+     "rect": {
+      "x": 16,
+      "y": 53,
+      "width": 24,
+      "height": 24
+     },
+     "index": 16,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 8,
+       "frameIndex": 3,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    },
+    {
+     "name": "01 Button - Primary",
+     "rect": {
+      "x": 98,
+      "y": 564,
+      "width": 167,
+      "height": 36
+     },
+     "index": 17,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 8,
+       "frameIndex": 3,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    }
+   ],
+   "layout": null,
+   "tmpSolitBackground": false
+  },
+  {
+   "id": "51:155",
+   "groupIndex": 0,
+   "title": "Product4",
+   "image": "product4.png",
+   "index": 9,
+   "width": 375,
+   "height": 812,
+   "x": 2142,
+   "y": -407,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Button",
+     "rect": {
+      "x": 13,
+      "y": 717,
+      "width": 343,
+      "height": 58
+     },
+     "index": 18,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 9,
+       "frameIndex": 4,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    },
+    {
+     "name": "Icon/Chevron_Left",
+     "rect": {
+      "x": 16,
+      "y": 53,
+      "width": 24,
+      "height": 24
+     },
+     "index": 19,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 9,
+       "frameIndex": 3,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    },
+    {
+     "name": "01 Button - Primary",
+     "rect": {
+      "x": 98,
+      "y": 564,
+      "width": 167,
+      "height": 36
+     },
+     "index": 20,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 9,
+       "frameIndex": 3,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    }
+   ],
+   "layout": null,
+   "tmpSolitBackground": false
+  },
+  {
+   "id": "60:16",
+   "groupIndex": 0,
+   "title": "Sign In2",
+   "image": "sign-in2.png",
+   "index": 10,
+   "width": 375,
+   "height": 812,
+   "x": 2557,
+   "y": -417,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Input and button",
+     "rect": {
+      "x": 24,
+      "y": 509,
+      "width": 327,
+      "height": 74
+     },
+     "index": 21,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 10,
+       "frameIndex": 2,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    },
+    {
+     "name": "07 Profile avatar",
+     "rect": {
+      "x": 270,
+      "y": 56,
+      "width": 81,
+      "height": 110
+     },
+     "index": 22,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 10,
+       "frameIndex": 5,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    }
+   ],
+   "layout": null,
+   "tmpSolitBackground": false
+  },
+  {
+   "id": "63:62",
+   "groupIndex": 0,
+   "title": "AnotherMeal",
+   "image": "anothermeal.png",
+   "index": 11,
+   "width": 375,
+   "height": 812,
+   "x": 2972,
+   "y": -417,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Input and button",
+     "rect": {
+      "x": 24,
+      "y": 509,
+      "width": 327,
+      "height": 74
+     },
+     "index": 23,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 11,
+       "frameIndex": 2,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    },
+    {
+     "name": "Icon/Chevron_Left",
+     "rect": {
+      "x": 15,
+      "y": 67,
+      "width": 24,
+      "height": 24
+     },
+     "index": 24,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 11,
+       "frameIndex": 4,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    }
+   ],
+   "layout": null,
+   "tmpSolitBackground": false
+  }
+ ],
+ "startPageIndex": 1,
+ "totalImages": 12,
+ "groups": [
+  {
+   "id": "0:1",
+   "index": 0,
+   "name": "Page 1",
+   "backColor": "#1E1E1E"
+  }
+ ]
+}
